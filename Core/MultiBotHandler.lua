bf1607a1f5ced9a0aea7d7416a6113619fc15498
zeb139 <37308742+zeb139@users.noreply.github.com>
2025-09-26 14:40:19 -0400

    Removed unrelated code change

--- Core/MultiBotHandler.lua
+++ Core/MultiBotHandler.lua
@@ -335,16 +335,6 @@ MultiBot:SetScript("OnEvent", function()
 				tButton.doLeft(tButton)
 			end
 		end
-		
-        if (MultiBotSave["Visible"] ~= nil) then
-            if (MultiBotSave["Visible"] == "true") then
-                for key, value in pairs(MultiBot.frames) do value:Show() end
-		        MultiBot.state = true
-            else
-                for key, value in pairs(MultiBot.frames) do value:Hide() end
-		        MultiBot.state = false
-            end
-        end
 
         if (MultiBotGlobalSave["Strata.Level"] ~= nil) then
             MultiBot.PromoteFrame(MultiBot.frames["MultiBar"], MultiBotGlobalSave["Strata.Level"])
@@ -1387,11 +1377,9 @@ SlashCmdList["MULTIBOT"] = function()
 	if(MultiBot.state) then
 		for key, value in pairs(MultiBot.frames) do value:Hide() end
 		MultiBot.state = false
-        MultiBotSave["Visible"] = "false"
 	else
 		for key, value in pairs(MultiBot.frames) do value:Show() end
 		MultiBot.state = true
-        MultiBotSave["Visible"] = "true"
 	end
 end
 
