91a384df22223b81963e568afeb769f8d26d7a8b
Wishmaster117 <140754794+Wishmaster117@users.noreply.github.com>
2025-12-04 11:25:55 +0100

    remove spaces

--- Features/MultiBotReward.lua
+++ Features/MultiBotReward.lua
@@ -5,20 +5,20 @@ MultiBot.setRewards = function()
 	table.wipe(MultiBot.reward.units)
 	MultiBot.reward.rewards = {}
 	MultiBot.reward.units = {}
-	
+
 	for i = 1, tChoices do
 		local tLink = GetQuestItemLink("CHOICE", i)
 		local tName, tIcon = GetQuestItemInfo("CHOICE", i)
 		MultiBot.reward.rewards[i] = { tLink, tName, tIcon }
 	end
-	
+
 	for i = 1, 12 do
 		local tID = "U" .. MultiBot.IF(i < 10, "0", "") .. i
 		local tUnit = MultiBot.reward.frames["Overlay"].frames[tID]
 		for j = 1, 6 do tUnit.buttons["R" .. j]:Hide() end
 		tUnit:Hide()
 	end
-	
+
 	if(GetNumRaidMembers() > 0) then
 		for i = 1, 40 do
 			local tUnit = UnitName("raid" .. i)
@@ -36,25 +36,25 @@ MultiBot.setRewards = function()
 			end
 		end
 	end
-	
+
 	if(table.getn(MultiBot.reward.units) > 0 and tChoices > 0) then
 		local tOverlay = MultiBot.reward.frames["Overlay"]
 		local tUnits = table.getn(MultiBot.reward.units)
-		
+
 		MultiBot.reward.max = math.ceil(tUnits / MultiBot.reward.to)
 		tOverlay.setText("Pages", MultiBot.reward.now .. "/" .. MultiBot.reward.max)
 		tOverlay.buttons["<"]:Show()
 		tOverlay.buttons[">"]:Show()
-		
+
 		if(MultiBot.reward.now == 1) then tOverlay.buttons["<"]:Hide() end
 		if(MultiBot.reward.now == MultiBot.reward.max) then tOverlay.buttons[">"]:Hide() end
-		
+
 		if(tUnits > MultiBot.reward.to) then tUnits = MultiBot.reward.to end
-		
+
 		for i = 1, tUnits do
 			local tBot = MultiBot.reward.units[i]
 			local tUnit = MultiBot.setReward(i, tBot, false)
-			
+
 			for j = 1, tChoices do
 				local tReward = tUnit.buttons["R" .. j]
 				tReward:Show()
@@ -68,7 +68,7 @@ MultiBot.setRewards = function()
 				end
 			end
 		end
-		
+
 		MultiBot.reward:Show()
 	end
 end
